d0445f79a6bfdb2820eeec4504e6842e054d32ef
louismarielefer <73694147+louismarielefer@users.noreply.github.com>
2020-11-16 10:59:53 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -29,4 +29,34 @@ bubble(meuse,"zinc")
 #do the same for lead and change color
 bubble(meuse,"lead", col="red")
 
+############ new course
+#install the ggplot2 package
+install.packages("ggplot2")
+library(ggplot2)
 
+#ecological data frame
+#biofuels
+biofuels <- c(120,200, 350, 570, 750) #array of values
+biofuels
+#oxydative enzymes
+oxydative <- c(1200, 1300, 21000,34000,50000)
+oxydative
+
+#dataframe
+d <- data.frame(biofuels, oxydative)
+d
+
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_point() #precise x and y
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_point(col="red", size=2)
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_line() #pour relier les points entre eux
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_point(col="red", size=2) + geom_line() #points and lines at the same time, here we have 3 elements added together
+
+#polygons
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_polygon() #pratique pour comparer avec une régression linéaire, if the area is bigger, the relationship is not linear
+
+########################################### LET'S IMPORT DATA !
+
+#for Windows
+setwd("C:/lab/")
+covid <- read.table("covid_agg.csv", head=T)
+covid
