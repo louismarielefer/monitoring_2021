2cd2232753f7e3fbf38853e42e0adf1d0c8ff516
louismarielefer <73694147+louismarielefer@users.noreply.github.com>
2020-12-11 10:15:25 +0100

    Create multivariate_analysis.r

---
+++ multivariate_analysis.r
@@ -0,0 +1,5 @@
+# we can see in 3D
+#maybe in 3D we can see things which don't seem related to each other but maybe they are in 4D
+#multivariate analyses is used to reduce the dimensionality and see things we wouldn't be able to see otherwise
+#how ? by squizzing dimensions
+#one example of squizzing is principle component (PC1 and PC2): from 2 axis to one
