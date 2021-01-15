83e2be8b757c2a599ca2d82977a92fecc90a219a
louismarielefer <73694147+louismarielefer@users.noreply.github.com>
2021-01-15 10:16:23 +0100

    Update and rename R_code_biomass.r to R_code_energy.r

--- R_code_energy.r
+++ R_code_energy.r
@@ -1,7 +1,7 @@
 #2 more important functions in a an ecosystem: energy flow and chemical cycle
 #1st part: energy flow
-plants reflect a lot NIR(near infrared red) and not a lot of red (because red lenghts are absorbed for photosynthesis)
-#DIV(indice of biomass)= NIR-RED
+#plants reflect a lot NIR(near infrared red) and not a lot of red (because red lenghts are absorbed for photosynthesis)
+#DVI(difference vegetation indice)(indice of biomass)= NIR-RED
 #for each zone, a DIV is given
 #for each line,lattitude, mean of values
 #make of a worldmap with graph on the side which shows where the indice of biomass is the higher
@@ -10,4 +10,6 @@ plants reflect a lot NIR(near infrared red) and not a lot of red (because red le
 #ADVICE FOR FINAL PROJECT
 #either: on a slide, put code on the left and output on the right
 #or change slide for output
+####################################################
+
 
