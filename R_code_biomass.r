421668eb4417996721011550f5f57fdb19d71f00
louismarielefer <73694147+louismarielefer@users.noreply.github.com>
2021-01-15 10:04:46 +0100

    Create R_code_biomass.r

---
+++ R_code_biomass.r
@@ -0,0 +1,13 @@
+#2 more important functions in a an ecosystem: energy flow and chemical cycle
+#1st part: energy flow
+plants reflect a lot NIR(near infrared red) and not a lot of red (because red lenghts are absorbed for photosynthesis)
+#DIV(indice of biomass)= NIR-RED
+#for each zone, a DIV is given
+#for each line,lattitude, mean of values
+#make of a worldmap with graph on the side which shows where the indice of biomass is the higher
+#second part:chemical cycle
+#comparison amount of NO2 in europe between january and march (lockdown)
+#ADVICE FOR FINAL PROJECT
+#either: on a slide, put code on the left and output on the right
+#or change slide for output
+
