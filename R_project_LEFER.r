ff15968ded555807a49aa6c250de1553c0517f3f
louismarielefer <73694147+louismarielefer@users.noreply.github.com>
2021-02-05 14:22:55 +0100

    Update R_project_LEFER.r

--- R_project_LEFER.r
+++ R_project_LEFER.r
@@ -1,4 +1,3 @@
-library(sp)
 library(spatstat)
 setwd("C:/lab/")
 
@@ -15,15 +14,11 @@ fire_austr_2019_planar <- ppp(longitude,latitude,c(113,154),c(-43,-9)) #ppp plan
 
 density_map <- density(fire_austr_planar)
 
-plot(density_map) 
-
-points(fire_austr_2019_planar) #make appear points of countries
-
 #putting countries boundaries on top of the map
 library(rgdal)
 
 coastlines <- readOGR("ne_10m_coastline.shp")
 
 plot(density_map, col=cl)
-points(covid_planar, pch=19, cex=0.5)
+points(covid_planar, pch=20, cex=0.5)
 plot(coastlines, add=TRUE)
